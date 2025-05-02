86dead7d5402bfe26435fbef392daedbea167a1a
Oleg Kornienko <kornienko340@gmail.com>
2025-05-02 17:03:40 +0300

    Update kino.js

--- kino.js
+++ kino.js
@@ -27,19 +27,44 @@
 
         // Функция добавляет надпись Hello в шапку
         function addHelloToHeader() {
-            var html$g;
-            console.log("qweqwe");
             let hello = $('<div class="card selector layer--visible layer--render card--loaded"><div class="card__view"><img src="http://tmdbimg.rootu.top/t/p/w500//tgSmrSZHMtI0PFBiDIrjfRV3qAm.jpg" class="card__img"><div class="card__icons"><div class="card__icons-inner"></div></div><div class="card__vote">6.6</div></div><div class="card__title">Опустошение</div><div class="card__age">2025</div></div>');
             $('.head .head__body').append(hello); // Вставляем в левую часть шапки
 
-            html$g = Lampa.Template.get('head');
-            console.log(html$g);
-            html$g.find('.card--loaded').on('hover:enter', function () {
+            $('.head .card--loaded').on('hover:enter', function () {
                 
-            console.log("Testtt");
-              ParentalControl.personal('settings', function () {
-                Controller.toggle('settings');
-              }, false, true);
+            Lampa.Activity.push({
+    url: undefined,
+    component: 'full',
+    id: 40290,
+    method: 'tv',
+    card: {
+        "adult": false,
+        "backdrop_path": "/vlg654HjwNc1J8bmjBtmRxgLs36.jpg",
+        "genre_ids": [
+            10764
+        ],
+        "id": 40290,
+        "origin_country": [
+            "US"
+        ],
+        "original_language": "en",
+        "original_name": "MasterChef",
+        "overview": "Огромное количество кулинаров принимает участие в состязаниях, главным призом которых является статус «Лучшего повара Америки». Помимо этого, они получают 250 000 долларов, а также возможность за счет издательства выпустить книгу, в которой будут представлены их рецепты. Судят участников лучшие кулинары. Рекомендуем к просмотру! Шоу вам непременно понравится.",
+        "popularity": 25.1628,
+        "poster_path": "/42gO1qCC6GPtHKNXvOdhunhzyS3.jpg",
+        "first_air_date": "2010-07-27",
+        "name": "Лучший повар Америки",
+        "vote_average": 7.075,
+        "vote_count": 272,
+        "ready": true,
+        "title": "Лучший повар Америки",
+        "original_title": "MasterChef",
+        "release_date": "2010-07-27",
+        "release_year": "2010",
+        "source": "tmdb"
+    },
+    source: 'tmdb'
+  });
             });
         }
 
